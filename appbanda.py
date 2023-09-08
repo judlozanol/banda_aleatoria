d360fd3b701576323bc628eb41cea6aa823d4de7
Juan Diego Lozano Luna <judlozanol@udistrital.edu.co>
2023-09-08 18:00:20 -0500

    no pone imagen pero en POO

--- appbanda.py
+++ appbanda.py
@@ -3,13 +3,29 @@ from banda import Banda
 from imagen import Imagen
 class AppBanda:
     def __init__(self):
-        self.banda = Banda()
         self.ventana= Tk()
         self.titulo= Label(self.ventana, text="Banda Aleatoria",font=("Helvetica",50), fg="dark green")
         self.canva= Canvas(width=400,height=300, bg="white")
-        self.generar= Button(self.ventana, text="Generar Banda", fg="black",font=("Helvetica",20))
+        self.generar= Button(self.ventana, text="Generar Banda", fg="black",font=("Helvetica",20), command=self.colocar_labels)
         self.afinar=Button(self.ventana, text="Afinar Banda", fg="black",font=("Helvetica",10) )
         self.tocar= Button(self.ventana, text="Tocar\n Instrumentos", fg="black",font=("Helvetica",10))
+        self.labels=[]
+    def crear_label(self, imagen:type[Imagen]):
+        return Label(self.canva, image=imagen.img)
+    def colocar_labels(self):
+        banda=Banda()
+        banda.generar_musicos()
+        x1=10
+        y1=8
+        for i in range(banda.numero_musicos):
+            imagen=Imagen(banda.musicos[i])
+            label= self.crear_label((imagen))
+            self.labels.append(label)
+            self.labels[i].place(x=x1, y=y1)
+            y1+=58
+            if y1>=298:
+                y1=8
+                x1+=195
     def ejecutar(self):
         self.ventana.geometry("800x600")
         self.ventana.title("Banda Aleatoria")
@@ -20,8 +36,6 @@ class AppBanda:
         self.generar.pack(pady=5)
         self.afinar.place(x=250, y=525)
         self.tocar.place(x=450, y=525)
-
-
 if __name__=="__main__":
     p=AppBanda()
     p.ejecutar()
--- imagen.py
+++ imagen.py
@@ -1,6 +1,6 @@
+from musico import Musico
 from tkinter import PhotoImage
-from tkinter import Label
-
 class Imagen:
-    def __init__(self):
-        pass
+    def __init__(self, musico:type[Musico]):
+        self.direccion_memoria= musico.instrumento.imagen
+        self.img= PhotoImage(file=self.direccion_memoria)
