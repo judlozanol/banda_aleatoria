537bafa3efd024a913cdaa553ee4e0e7fb1f49d1
Juan Diego Lozano Luna <judlozanol@udistrital.edu.co>
2023-09-08 19:33:09 -0500

    ejercicio funcional(solo imagenes):D

--- appbanda.py
+++ appbanda.py
@@ -9,17 +9,22 @@ class AppBanda:
         self.generar= Button(self.ventana, text="Generar Banda", fg="black",font=("Helvetica",20), command=self.colocar_labels)
         self.afinar=Button(self.ventana, text="Afinar Banda", fg="black",font=("Helvetica",10) )
         self.tocar= Button(self.ventana, text="Tocar\n Instrumentos", fg="black",font=("Helvetica",10))
-        self.labels=[]
     def colocar_labels(self):
+        global imagen
+        global imagenes
+        imagenes=[]
+        labels=[]
         banda=Banda()
         banda.generar_musicos()
+        for i in range(banda.numero_musicos):
+            imagen=PhotoImage(file=banda.musicos[i].instrumento.memoria)
+            imagenes.append(imagen)
         x1=10
         y1=8
         for i in range(banda.numero_musicos):
-            imagen=PhotoImage(file=banda.musicos[i].instrumento.memoria)
-            label= Label(self.canva, image=imagen)
-            self.labels.append(label)
-            self.labels[i].place(x=x1, y=y1)
+            label= Label(self.canva, image=imagenes[i])
+            labels.append(label)
+            labels[i].place(x=x1, y=y1)
             y1+=58
             if y1>=298:
                 y1=8
@@ -34,9 +39,9 @@ class AppBanda:
         self.generar.pack(pady=5)
         self.afinar.place(x=250, y=525)
         self.tocar.place(x=450, y=525)
-        hola=PhotoImage(file="guitarra.png")
-        adios= Label(self.ventana, image=hola).place(x=1,y=1)
+        
 if __name__=="__main__":
     p=AppBanda()
     p.ejecutar()
     p.ventana.mainloop()
+    
