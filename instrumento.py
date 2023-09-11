7d1b8218649fbc348c544604d1a03b382611cb22
Juan Diego Lozano Luna <141884950+judlozanol@users.noreply.github.com>
2023-09-11 07:02:46 -0500

    Update instrumento.py

--- instrumento.py
+++ instrumento.py
@@ -5,7 +5,7 @@ class Instrumento():
         self.memoria=str
     def afinar(self):
         if self.afina==False:
-            return (self.nombre+" no se\npuede afinar.")
+            return (self.nombre+" no se puede afinar.")
         else:
             return("Afinando "+ self.nombre+"...")
     def tocar(self):
